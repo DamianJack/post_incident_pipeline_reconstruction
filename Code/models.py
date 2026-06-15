fix: update Trainer class to include precision, recall, and F1 score metrics for test set; refactor variable names for clarity

Code/models.py
@@ -1,8 +1,9 @@
-"""
-MAI/IDL SS26 - Final assignment. 
+#----------------------
+# AUTHORS:
+# DAMIAN - 10012545
+# BHUVAN - 10001026
+#-----------------------
 
-MG 6/6/2026
-"""
 import torch
 import torch.nn as nn
 