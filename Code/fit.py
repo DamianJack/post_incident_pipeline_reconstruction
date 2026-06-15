fix: update Trainer class to include precision, recall, and F1 score metrics for test set; refactor variable names for clarity

Code/fit.py
@@ -5,6 +5,7 @@
 #-----------------------
 
 import torch
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 class Trainer:
     def __init__(self, model, criterion, optimizer, device):
@@ -16,7 +17,7 @@ class Trainer:
     def train_one_epoch(self, dataloader):
         self.model.train()
         running_loss = 0.0
-        correct, sum = 0, 0
+        correct, total = 0, 0
         
         for images, labels in dataloader:
             images, labels = images.to(self.device), labels.to(self.device)
@@ -30,10 +31,10 @@ class Trainer:
             
             running_loss += loss.item() * images.size(0)
             _, predicted = outputs.max(1)
-            sum += labels.size(0)
+            total += labels.size(0)
             correct += predicted.eq(labels).sum().item()
             
-        return running_loss / sum, (correct / sum) * 100
+        return running_loss / total, (correct / total) * 100
 
     def evaluate(self, dataloader):
         self.model.eval()
@@ -68,3 +69,29 @@ class Trainer:
         
         print("-" * 50)
         print("Training Complete!")
+
+    def test_eval(self, dataloader):
+        self.model.eval()
+        preds, targets = [], []
+        correct, total = 0, 0
+        
+        with torch.no_grad():
+            for images, labels in dataloader:
+                images, labels = images.to(self.device), labels.to(self.device)
+    
+                outputs = self.model(images)
+                preds = outputs.argmax(dim=1)
+    
+                preds.extend(preds.cpu().tolist())
+                targets.extend(labels.cpu().tolist())
+
+                correct += preds.eq(labels).sum().item()
+                total += labels.size(0)
+    
+        precision = precision_score(targets, preds, average="macro")
+        recall    = recall_score(targets, preds, average="macro")
+        macro_f1  = f1_score(targets, preds, average="macro")
+        accuracy  = (correct / total) * 100
+
+    
+        return precision, recall, macro_f1, accuracy

Code/train.py
@@ -1,11 +1,11 @@
-"""
-MAI/IDL SS26 - Final assignment. 
+#----------------------
+# AUTHORS:
+# DAMIAN - 10012545
+# BHUVAN - 10001026
+#-----------------------
 
-MG 6/6/2026
-"""
 import json
 
-from networkx import config
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -20,15 +20,19 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Training executing on device: {device}")
 
-    train_loader, val_loader, _ = get_loaders(data=config["DATA"], data_path=config["DATA_PATH"], batch_size=config["BATCH_SIZE"])
+    train_loader, val_loader, test_loader = get_loaders(data=config["DATA"], data_path=config["DATA_PATH"], batch_size=config["BATCH_SIZE"])
 
     model_class = getattr(models, config["MODEL"])
-    model = model_class(in_channels=config["CHANNELS"], num_classes=config["NUM_CLASSES"], drop_rate=0.5, activation_str=config["ACTIVATION"]).to(device)
+    model = model_class(in_channels=config["CHANNELS"], num_classes=config["NUM_CLASSES"], drop_rate=config["DROP_RATE"], activation_str=config["ACTIVATION"]).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=config["LEARNING_RATE"])
 
     trainer = Trainer(model, criterion, optimizer, device)
     trainer.fit(train_loader, val_loader, epochs=config["EPOCHS"])
+    trainer.save_checkpoint("best_model.pth")
+    trainer.load_checkpoint("best_model.pth")
+    precision, recall, macro_f1 = trainer.test(test_loader)
+    print(f"\nTest Precision: {precision:.4f}, Recall: {recall:.4f}, Macro F1-Score: {macro_f1:.4f}")
 
 if __name__ == "__main__":
     main()