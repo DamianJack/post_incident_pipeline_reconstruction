646454d953f59a108ddda2ccfa505279dc94e49f
Thota Bhuvana Chandra <thotab14@gmail.com>
2026-07-07 12:19:31 +0200

    fix: update validation split to 0.2 in get_loaders function for organs

--- Code/data.py
+++ Code/data.py
@@ -12,7 +12,7 @@ from torch.utils.data import TensorDataset, DataLoader
 
 logger = logging.getLogger(__name__)
 
-def get_loaders(data, data_path, batch_size, val_split=0.1):
+def get_loaders(data, data_path, batch_size, val_split=0.2):
 
     d_path    = Path(data_path) / f"{data}.pt"
     data_dict = torch.load(d_path,weights_only=True)
